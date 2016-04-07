d0ec3666abf2188295e0578f0d624e7b6894b749
Lokesh Dokara <lokeshwithuever@gmail.com>
2016-04-07 11:24:52 +0530

    Initial Commit

---
+++ ui/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+def index(request):
+    return render(request, 'login.html', {})
