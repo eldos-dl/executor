e7888c19b714cbb4ea4b8132dd9bde44c6ce592d
Lokesh Dokara <lokeshwithuever@gmail.com>
2016-04-08 11:17:20 +0530

    System Stats at /stats/

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "executor.settings")
+    os.environ.setdefault("NO_PROXY", "10.*.*.*,localhost,127.0.0.1")
 
     from django.core.management import execute_from_command_line
 
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+import json
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
-# Create your views here.
+
+def get_stats(request):
+    import psutil
+    memory = psutil.virtual_memory()
+    stats = {
+        'memory': {
+             'available': memory.available,
+             'total': memory.total,
+             'used_percent': memory.percent
+        },
+        'cpu': {
+            'count': psutil.cpu_count(),
+            'used_percent': psutil.cpu_percent(),
+            'idle_percent': psutil.cpu_times_percent().idle
+        }
+    }
+    return JsonResponse(stats)
